dfa4a2bbe59bcc8ba21a2bb461afe932e0ea7c9f
skyoo1226 <you1226@naver.com>
2022-03-05 11:55:28 +0900

    다른 화면으로 이동...

--- app/src/main/java/com/neppplus/lifecycle_20220305/MainActivity.kt
+++ app/src/main/java/com/neppplus/lifecycle_20220305/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.neppplus.lifecycle_20220305
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+        fun setupEvents()  {
+
+            btnMove.setOnClickListener {
+
+                val myIntent = Intent(this, OtherActivity::class.java)
+                startActivity(myIntent)
+
+            }
+        }
+
+        fun setValues() {
+            Log.d("메인화면", "onCreate 실행됨")
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/neppplus/lifecycle_20220305/OtherActivity.kt
@@ -0,0 +1,14 @@
+package com.neppplus.lifecycle_20220305
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+
+class OtherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_other)
+
+        Log.d("다른", "onCreate가 실행됨")
+    }
+}
